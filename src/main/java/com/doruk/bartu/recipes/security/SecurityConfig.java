f1ba76dc8ec59e46ef7c5c490d502e842087dc15
NovasSs19 <dorukhan1904@hotmail.com>
2026-01-27 03:23:49 +0100

    LAST TEST fix  before presentation

--- src/main/java/com/doruk/bartu/recipes/security/SecurityConfig.java
+++ src/main/java/com/doruk/bartu/recipes/security/SecurityConfig.java
@@ -28,6 +28,7 @@ public class SecurityConfig {
                         .requestMatchers(
                                 "/",
                                 "/index.html",
+                                "/csrf",
                                 "/auth/**",
                                 "/css/**",
                                 "/js/**",
